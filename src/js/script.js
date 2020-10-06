1f59b3b726bf07c5b792039465f99adf389698b0
Hera Gratton <sonicrock4ever@gmail.com>
2020-10-06 18:22:24 -0400

    Ajout SCSS et JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburgers = document.querySelectorAll('.hamburger');
+    var menuMobile = document.querySelector('.nav-primary-mobile');
+
+
+    for(let i = 0; i < hamburgers.length; i++) {
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener('click', openMenu);
+    }
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var target = evt.currentTarget;
+
+        if(target.classList.contains('open')) {
+            target.classList.remove('open');
+            menuMobile.classList.remove('open');
+        } else {
+            target.classList.add('open');
+            menuMobile.classList.add('open');
+
+        }
+    }
 });
